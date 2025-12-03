6a94a78eaa202ac65448f5c784a2049e3b453cd5
Gre882at <adeniyigreat798@gmail.com>
2025-12-03 11:50:59 +0100

    Add files via upload

---
+++ Libs_py/Libs.py
@@ -0,0 +1,18 @@
+Nounname = input('Enter a noun:')
+print()
+Name = input ('Enter a verb:') 
+print()
+Animalname = input ('Enter an adjective:')
+print()
+Placename = input ('Enter a place:')
+print()
+print("The " + Animalname + ' ' + Nounname + ' decided to ' + Name + ' in the ' + Placename + '. Everyone laughed and cheered!' )
+
+
+
+# Line 1 asks for the users input for a noun.
+# Line 2, 4, 6, and 8 are to used to give the questions space.
+# Line 3 asks for the users input for a verb.
+# Line 5 asks the user to input an adjective.
+# Line 8 asks the user for a place where the activity would take place.
+# Line 9: This is the final print statement that creates the story.
